2349fcb80732d2137727726fe37aafad6054d8de
rookless <rooklesscs@gmail.com>
2024-11-09 21:18:38 +0100

    Miny działają w pełni (chyba)
    Jak ktoś ma czas to może się pobawić chwilę i potestować + css jest do zrobienia + główny css jest rozjebany + zostawiłem komentarze jakbyście coś robili (html i css możecie przebudować kompletnie a js to możecie dodać coś ale starajcie się nie ruszać xd)

--- minesGame/minesScript.js
+++ minesGame/minesScript.js
@@ -6,25 +6,69 @@ const betBtn = document.getElementById("betBtn");
 const gameInfo = document.getElementById("gameInfo");
 const randomTileBtn = document.getElementById("randomTileBtn");
 const cashoutBtn = document.getElementById("cashoutBtn");
+const minesRemaining = document.getElementById("minesRemaining");
+const gemsRemaining = document.getElementById("gemsRemaining");
+const profitLabel = document.getElementById("profitLabel");
+const profit = document.getElementById("profit");
+const gameResult = document.getElementById("gameResult");
+const playAgainBtn = document.getElementById("playAgainBtn");
+
+    // gems, mines
+    
+const multiplier = [ 
+    [1.01, 1.08, 1.12, 1.18, 1.24, 1.3, 1.37, 1.46, 1.55, 1.65, 1.77, 1.9, 2.06, 2.25, 2.47, 2.75, 3.09, 3.54, 4.12, 4.95, 6.19, 8.25, 12.37, 24.75], 
+    [1.08, 1.17, 1.29, 1.41, 1.56, 1.74, 1.94, 2.18, 2.47, 2.83, 3.26, 3.81, 4.5, 5.4, 6.6, 8.25, 10.61, 14.14, 19.8, 29.7, 49.5, 99, 297],
+    [1.12, 1.29, 1.48, 1.71, 2, 2.35, 2.79, 3.35, 4.07, 5, 6.26, 7.96, 10.35, 13.8, 18.97, 27.11, 40.66, 65.06, 113.85, 227.7, 569.3, 2277],
+    [1.18, 1.41, 1.71, 2.09, 2.58, 3.23, 4.09, 5.26, 6.88, 9.17, 12.51, 17.52, 25.3, 37.95, 59.64, 99.39, 178.91, 357.81, 834.9, 2504, 12523],
+    [1.24, 1.56, 2, 2.58, 3.39, 4.52, 6.14, 8.5, 12.04, 17.52, 26.77, 40.87, 66.41, 113.85, 208.72, 417.45, 939.26, 2504, 12523],
+    [1.3, 1.74, 2.35, 3.23, 4.52, 6.46, 9.44, 14.17, 21.89, 35.03, 58.38, 102.17, 189.75, 379.5, 834.9, 2087, 6261, 25047, 175329],
+    [1.37, 1.94, 2.79, 4.09, 6.14, 9.44, 14.95, 24.47, 41.6, 73.95, 138.66, 277.33, 600.87, 1442, 3965, 13219, 59486, 475893],
+    [1.46, 2.18, 3.35, 5.26, 8.5, 14.17, 24.47, 44.05, 83.2, 166.4, 356.56, 831.98, 2163, 6489, 23794, 118973, 1070759],
+    [1.55, 2.47, 4.07, 6.88, 12.04, 21.89, 41.6, 83.2, 176.8, 404.1, 1010, 2828, 9193, 36773, 202254, 2022545],
+    [1.65, 2.83, 5, 9.17, 17.52, 35.03, 73.95, 166.4, 404.1, 1077, 3232, 11314, 49031, 294188, 3236072],
+    [1.77, 3.26, 6.26, 12.51, 26.27, 58.38, 138.66, 356.56, 1010, 3232, 12123, 56574, 367735, 4412826],
+    [1.99, 3.81, 7.96, 17.52, 40.87, 102.17, 277.33, 831.98, 2828, 11314, 56574, 396022, 5148297],
+    [2.06, 4.5, 10.35, 25.3, 66.41, 189.75, 600.87, 2163, 9193, 49031, 367735, 5148297],
+    [2.25, 5.4, 13.8, 37.95, 113.9, 379.5, 1442, 6489, 36773, 294188, 4412826],
+    [2.47, 6.6, 18.97, 59.64,208.7, 834.9, 3965, 23794, 202254, 3236072],
+    [2.75, 8.25, 27.11, 99.39, 417.5, 2087, 13219, 118973, 2022545],
+    [3.09, 10.61, 40.66, 178.9, 939.3, 6261, 59486, 1070759],
+    [3.54, 14.14, 65.06, 357.8, 2504, 25047, 475893],
+    [4.12, 19.8, 133.9, 834.9, 8766, 175329],
+    [4.95, 29.7, 227.7, 2504, 52598],
+    [6.19, 49.5, 569.3, 12523],
+    [8.25, 99, 2277],
+    [12.38, 297],
+    [24.75]
+];
 
 let hasBet = false;
 let balance = 1000;
 let mines = [];
+let gemsNumber;
+let gemsClicked;
 
 function gameStart() {
+
     for (i = 0; i < tiles.length; i++) {
         document.getElementById(tiles[i]).innerHTML = "";
     }
     if (betInput.value > balance || betInput.value < 0.01) {
         return;
     } 
-
     hasBet = true;
     balance = balance - betInput.value;
     betInput.readOnly = true;
+    gemsClicked = 0;
+    profitLabel.innerHTML = "Profit (x1)"
+    profit.value = betInput.value;
     changeStyle("game");
+    minesRemaining.value = minesInput.value;
+    gemsNumber = 25 - minesInput.value;
+    gemsRemaining.value = gemsNumber;
 
     while(mines.length < minesInput.value) {
+                                                                                                                            // Mine Generation
         let mineId = Math.floor(Math.random() * 25) + 1;
         if (!mines.includes(mineId)) {
             mines.push(mineId);
@@ -34,16 +78,32 @@ function gameStart() {
 }
 
 function tileClick(tileId) {
+    
+
     if (!hasBet) {
         return;
     }
     if (mines.includes(tileId)) {
+                                                                                                                            // Mine Clicked
         document.getElementById(tileId).innerHTML= "<img src='mine.svg'>";
         hasBet = false;
         betInput.readOnly = false;
-        changeStyle()
+        mines = [];
+        changeStyle("gameLost");
     } else {
+                                                                                                                            // Gem Clicked
+        gemsNumber--;
+        gemsClicked++;
+        console.log(gemsClicked);
+        gemsRemaining.value = gemsNumber;
         document.getElementById(tileId).innerHTML = "<img src='gem.svg'>";
+        profitLabel.innerHTML = `Profit (x${multiplier[gemsClicked - 1][mines.length - 1]})`;
+        profit.value = (betInput.value * multiplier[gemsClicked - 1][mines.length - 1]).toFixed(2);
+                                                                                                                            // Game Won 
+        if (gemsClicked == 25 - mines.length) {
+            cashout();
+            changeStyle("gameWon");
+        }
     }
 }
 
@@ -73,6 +133,10 @@ function changeStyle(style) {
             gameInfo.style.display = "none";
             randomTileBtn.style.display = "none";
             cashoutBtn.style.display = "none";
+            profitLabel.style.display = "none";
+            profit.style.display = "none";
+            gameResult.style.display = "none"
+            playAgainBtn.style.display = "none";
             break;
         case "game":
             minesInput.style.display = "none";
@@ -81,6 +145,54 @@ function changeStyle(style) {
             gameInfo.style.display = "initial";
             randomTileBtn.style.display = "initial";
             cashoutBtn.style.display = "initial";
+            profitLabel.style.display = "initial";
+            profit.style.display = "initial";
+            gameResult.style.display = "none";
+            playAgainBtn.style.display = "none";
+            break;
+        case "gameWon":
+            minesInput.style.display = "none";
+            minesLabel.style.display = "none";
+            betBtn.style.display = "none";
+            gameInfo.style.display = "initial";
+            randomTileBtn.style.display = "initial";
+            cashoutBtn.style.display = "initial";
+            profitLabel.style.display = "initial";
+            profit.style.display = "initial";
+            gameResult.innerHTML = "You Won!";
+            gameResult.style.display = "initial";
+            playAgainBtn.style.display = "initial";
+            break;
+        case "gameLost":
+            minesInput.style.display = "none";
+            minesLabel.style.display = "none";
+            betBtn.style.display = "none";
+            gameInfo.style.display = "none";
+            randomTileBtn.style.display = "none";
+            cashoutBtn.style.display = "none";
+            profitLabel.style.display = "none";
+            profit.style.display = "none";
+            gameResult.innerHTML = "You Lost!";
+            gameResult.style.display = "initial";
+            playAgainBtn.style.display = "initial";
+            break;
     }
+}
+
+function randomTile () {
+    let tileGotClicked = false;
+    while(!tileGotClicked) {
+        let pickedTileId = Math.floor(Math.random() * 25) + 1;
+        if (document.getElementById(pickedTileId).innerHTML == ""){
+            tileClick(pickedTileId);
+            tileGotClicked = true;
+        }
+    }
+}
+
+function cashout() {
+    changeStyle("initial");
+    hasBet = false;
+    balance = parseFloat(balance) + parseFloat(profit.value);
 }
 changeStyle("initial");
